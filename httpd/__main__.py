c903c8130e04c90bf923ac994f22c928077f3c05
Gerald <gera2ld@163.com>
2015-08-19 00:47:36 +0800

    Add arguments to facilitate `python -m` run mode

--- httpd/__main__.py
+++ httpd/__main__.py
@@ -5,15 +5,20 @@ from . import config, serve, __version__
 from .log import logger
 
 if __name__=='__main__':
+    import argparse, sys
     logger.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     fmt = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
     ch.setFormatter(fmt)
     logger.addHandler(ch)
+    parser = argparse.ArgumentParser(description = 'HTTP server by Gerald.')
+    parser.add_argument('-p', '--port', default = 8000, help = 'the port for the server to bind')
+    parser.add_argument('-r', '--root', default = './', help = 'the root directory of documents, MUST ends with `/`')
     logger.info('HTTP Server v%s/%s %s - by Gerald'
             % (__version__, platform.python_implementation(), platform.python_version()))
     config.add_gzip(['text/html', 'text/css', 'application/javascript'])
-    server = config.add_server(port = 8000)
-    server.add_alias('/', '~/')
+    args = parser.parse_args()
+    server = config.add_server(port = args.port)
+    server.add_alias('/', './')
     serve()
