70414b0aca5ff1a93c5d3d00a0cdd3fd3ab86175
Gerald <gera2ld@163.com>
2016-03-02 21:25:35 +0800

    Update setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!python
 # coding=utf-8
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-	name='httpd',
-	version='1.0',
-	packages=['httpd'],
-	author='Gerald',
-	author_email='gera2ld@163.com',
-	description='Super light HTTP daemon in Python, using asyncio.',
-	url='https://github.com/gera2ld/pyhttpd',
+    name = 'httpd',
+    version = '1.0',
+    packages = find_packages(),
+    package_data = {
+        'httpd': ['templates/*.html'],
+    },
+    author = 'Gerald',
+    author_email = 'gera2ld@163.com',
+    description = 'Super light HTTP daemon in Python, using asyncio.',
+    url = 'https://github.com/gera2ld/pyhttpd',
 )
