1e40e1517f218e1560695843bb6b888a19cbde25
Gerald <gera2ld@163.com>
2016-06-10 17:49:12 +0800

    close file after file contents sent

--- httpd/handlers.py
+++ httpd/handlers.py
@@ -6,14 +6,16 @@ from . import config, template, fcgi
 
 class FileProducer:
     bufsize = 4096
+    fp = None
     def __init__(self, path, start = 0, length = None):
-        self.fp = open(path, 'rb')
-        if start: self.fp.seek(start)
+        if length is None or length > 0:
+            self.fp = open(path, 'rb')
+            if start: self.fp.seek(start)
         self.length = length
     def __iter__(self):
         return self
     def __next__(self):
-        if self.length is 0:
+        if self.fp is None:
             raise StopIteration
         length = min(self.length, self.bufsize) if self.length else self.bufsize
         data = self.fp.read(length)
@@ -23,6 +25,10 @@ class FileProducer:
             return data
         else:
             raise StopIteration
+    def __del__(self):
+        if self.fp is not None:
+            self.fp.close()
+            self.fp = None
 
 class BaseHandler:
     def __init__(self, parent):
