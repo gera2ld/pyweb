1b96a4601a981f593bffedda6aa8221094e3c618
Gerald <gera2ld@163.com>
2017-05-09 20:23:36 +0800

    fix: add `bind` option

--- httpd/__main__.py
+++ httpd/__main__.py
@@ -8,6 +8,25 @@ from . import __version__
 from .server import HTTPServer
 from .log import logger
 
+def parse_addr(hostname, default_host='', default_port=80):
+    if hostname.count(':') > 1:
+        # IPv6
+        if hostname.startswith('['):
+            end_offset = hostname.index(']')
+            host = hostname[1 : end_offset]
+            port = hostname[end_offset + 1].lstrip(':')
+        else:
+            host = hostname
+            port = default_port
+    else:
+        # IPv4
+        host, _, port = hostname.partition(':')
+    try:
+        port = int(port)
+    except:
+        port = default_port
+    return host, port
+
 def main():
     '''Start server.'''
     logger.setLevel(logging.INFO)
@@ -17,16 +36,21 @@ def main():
     handler.setFormatter(fmt)
     logger.addHandler(handler)
 
-    parser = argparse.ArgumentParser(description='HTTP server by Gerald.')
-    parser.add_argument('-p', '--port', default=8000, help='the port for the server to bind')
-    parser.add_argument('-r', '--root', default='.', help='the root directory of documents')
+    parser = argparse.ArgumentParser(prog='python3 -m httpd', description='HTTP server by Gerald.')
+    parser.add_argument(
+        '-b', '--bind', default=':8000',
+        help='the address to bind, default as `:8000`')
+    parser.add_argument(
+        '-r', '--root', default='.',
+        help='the root directory of documents')
     args = parser.parse_args()
 
     logger.info(
         'HTTP Server v%s/%s %s - by Gerald',
         __version__, platform.python_implementation(), platform.python_version())
 
-    server = HTTPServer(port=args.port)
+    host, port = parse_addr(args.bind, default_host='', default_port=8000)
+    server = HTTPServer(host=host, port=port)
     server.add_gzip(['text/html', 'text/css', 'application/javascript'])
     server.add_alias('/', args.root)
     HTTPServer.serve(server)
